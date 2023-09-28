4b25be81a88608fbc791365a9d0c79bbd09fe996
masudranadev <masudranadev51@gmail.com>
2023-09-28 13:10:09 +0600

    redux setup backend code setup all backend code ready done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer } from "./rootReducer";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 
